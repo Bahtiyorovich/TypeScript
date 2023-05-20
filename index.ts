e9e89a8f3ef63ef39143f6e39ae61715f5c660f8
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-05-20 14:54:43 +0500

    news

---
+++ index.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+interface User {
+    id: number,
+    title: string,
+    completed: boolean,
+}
+const logger = (id: number, title: string, completed: boolean) => {
+    console.log(`
+    My ID: ${id}
+    My Title: ${title}
+    My Status: ${completed}
+`)
+}
+axios.get(url).then(res => {
+    const user = res.data as User;
+    const id = user.id;
+    const title = user.title;
+    const completed = user.completed;
+
+   logger(id, title, completed)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
